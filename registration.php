931da61a67ce3e5b9613afb6f314f2f911340a4c
Felix <iamahzoolah@gmail.com>
2025-11-28 19:00:32 -0800

    Set up a basic registration form, making sure to take security measures against SQL injection now so I do not need to go back later.

---
+++ registration.php
@@ -0,0 +1,34 @@
+<?php
+
+// Included so I can use my $connection object
+include 'db.php';
+
+// Stores user input
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+    
+    // Hashing the password immediately after user input
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    // Preventing SQL injection by not storing the values here, and instead using placeholders
+    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    
+    // Statement object creation.
+    $stmt = $connection->prepare($sql);
+    
+    // Using bind_param as another layer of security, so SQL injection may not take place, only data
+    $stmt->bind_param("sss", $username, $email, $hashed_password);
+
+    //If all the user input is accepted, it will let them know they successfully registered. If not, it will explain why
+    if ($stmt->execute()) {
+        echo "Successfully registered!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    
+    $stmt->close();
+    $connection->close();
+}
+?>
